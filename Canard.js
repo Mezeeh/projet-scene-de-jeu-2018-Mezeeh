339ea2dedba62f784a6c8d5c051727d773d724ee
Mezeeh <mezeeh@hotmail.com>
2018-02-08 11:39:51 -0500

    LIVRAISON_INTERACTION
    Correction du code causé par le merge

--- Canard.js
+++ Canard.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-function Canard(scene, jeu){
-=======
 function Canard(scene){
->>>>>>> f7d756336dfa4da8fb3fc3b92d4cd2e6265b3b56
 	var imageCanard;
 	var canard = this;
 	this.estCharge = false;
@@ -10,11 +6,7 @@ function Canard(scene){
 	
 	function initialiser(){
 		imageCanard = new Image();
-<<<<<<< HEAD
 		imageCanard.src = "decoration/sprites/sprite-canard-volant.png";
-=======
-		imageCanard.src = "sprites/sprite-canard-volant.png";
->>>>>>> f7d756336dfa4da8fb3fc3b92d4cd2e6265b3b56
 		imageCanard.onload = terminerChargement;
 	}
 	
@@ -24,10 +16,7 @@ function Canard(scene){
 		{
 			images:[imageCanard],
 			frames:{width:331, height:307},
-<<<<<<< HEAD
 			framerate: 8,
-=======
->>>>>>> f7d756336dfa4da8fb3fc3b92d4cd2e6265b3b56
 			animations:
 			{
 				vole:[0,1,2,3,4,5,6,7]
@@ -36,11 +25,7 @@ function Canard(scene){
 		canard.estCharge = true;
 		animationCanardVole = new createjs.Sprite(spriteCanardVolant, "vole");
 	
-<<<<<<< HEAD
 		//animationCanardVole.on("click", jeu.arme.tirer());
-=======
-		animationCanardVole.on("click", exploser());
->>>>>>> f7d756336dfa4da8fb3fc3b92d4cd2e6265b3b56
     }
 	
 
@@ -53,10 +38,7 @@ function Canard(scene){
 	}
 	
 	this.exploser = function(){
-<<<<<<< HEAD
 		// TODO : animer une mort
-=======
->>>>>>> f7d756336dfa4da8fb3fc3b92d4cd2e6265b3b56
 		scene.removeChild(animationCanardVole);
 	}
     
